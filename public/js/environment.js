b8f3ac2771f9d6813a22276b87c73368b2016e85
AidanNelson <AidanNelson@users.noreply.github.com>
2021-03-04 21:03:56 -0500

    add fog to scene

--- public/js/environment.js
+++ public/js/environment.js
@@ -3,6 +3,10 @@ let myMesh;
 function createEnvironment(scene) {
   console.log("Adding environment");
 
+  console.log(scene);
+
+  scene.fog = new THREE.FogExp2( 0x232323, 0.025 );
+
   let texture = new THREE.TextureLoader().load("../assets/texture.png");
   let myGeometry = new THREE.SphereGeometry(3, 12, 12);
   let myMaterial = new THREE.MeshBasicMaterial({ map: texture });
